2ad9e5c8cfa7275d0c46546b0bf0fa3fbf608eb5
Alexsander Falcucci <alex.falcucci@gmail.com>
2026-05-20 19:24:27 +0200

    verifier: lower bounds before solving (#9)
    lower `must` expressions into typed verifier constraints, using the scope that
    belongs to the owner: model fields, state fields or fn parameters.
    
    now we keep this independent of any solver.

--- crates/verifier/src/lib.rs
+++ crates/verifier/src/lib.rs
@@ -1,8 +1,15 @@
+use std::collections::HashMap;
 use std::fmt;
 
 use parser::Span;
+use parser::ast;
 use semantics::Bounds;
 use semantics::ContractDefinition;
+use semantics::FieldDefinition;
+use semantics::FunctionDefinition;
+use semantics::ParameterDefinition;
+use semantics::StateDefinition;
+use semantics::Type;
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct VerificationReport {
@@ -10,14 +17,135 @@ pub struct VerificationReport {
     pub model_bounds: usize,
     pub state_bounds: usize,
     pub function_bounds: usize,
-    pub results: Vec<BoundVerification>,
+    pub constraints: Vec<Constraint>,
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
-pub struct BoundVerification {
+pub struct Constraint {
     pub owner: BoundOwner,
+    pub expression: VerifiedExpression,
     pub span: Span,
-    pub outcome: VerificationOutcome,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum VerifiedExpression {
+    IntLiteral {
+        text: String,
+        span: Span,
+    },
+    Identifier {
+        name: String,
+        ty: VerifiedType,
+        span: Span,
+    },
+    Binary {
+        lhs: Box<VerifiedExpression>,
+        op: VerifiedBinaryOperator,
+        rhs: Box<VerifiedExpression>,
+        ty: VerifiedType,
+        span: Span,
+    },
+}
+
+impl VerifiedExpression {
+    pub fn ty(&self) -> VerifiedType {
+        match self {
+            VerifiedExpression::IntLiteral { .. } => VerifiedType::IntegerLiteral,
+            VerifiedExpression::Identifier { ty, .. } | VerifiedExpression::Binary { ty, .. } => {
+                ty.clone()
+            }
+        }
+    }
+
+    pub fn span(&self) -> Span {
+        match self {
+            VerifiedExpression::IntLiteral { span, .. }
+            | VerifiedExpression::Identifier { span, .. }
+            | VerifiedExpression::Binary { span, .. } => *span,
+        }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum VerifiedType {
+    Int,
+    UInt,
+    Bool,
+    String,
+    Address,
+    Hex,
+    Custom(String),
+    IntegerLiteral,
+}
+
+impl VerifiedType {
+    fn is_numeric(&self) -> bool {
+        matches!(
+            self,
+            VerifiedType::Int | VerifiedType::UInt | VerifiedType::IntegerLiteral
+        )
+    }
+
+    fn is_compatible_with(&self, other: &Self) -> bool {
+        self == other || (self.is_numeric() && other.is_numeric())
+    }
+
+    fn merged_numeric(lhs: &Self, rhs: &Self) -> Self {
+        if lhs == &VerifiedType::Int || rhs == &VerifiedType::Int {
+            VerifiedType::Int
+        } else if lhs == &VerifiedType::UInt || rhs == &VerifiedType::UInt {
+            VerifiedType::UInt
+        } else {
+            VerifiedType::IntegerLiteral
+        }
+    }
+}
+
+impl From<&Type> for VerifiedType {
+    fn from(value: &Type) -> Self {
+        match value {
+            Type::Int => VerifiedType::Int,
+            Type::UInt => VerifiedType::UInt,
+            Type::Bool => VerifiedType::Bool,
+            Type::String => VerifiedType::String,
+            Type::Address => VerifiedType::Address,
+            Type::Hex => VerifiedType::Hex,
+            Type::Custom(name) => VerifiedType::Custom(name.clone()),
+        }
+    }
+}
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
+pub enum VerifiedBinaryOperator {
+    Equal,
+    NotEqual,
+    Greater,
+    GreaterEqual,
+    Less,
+    LessEqual,
+    Add,
+    Subtract,
+    Multiply,
+    Divide,
+    Modulo,
+}
+
+impl From<ast::BinaryOperator> for VerifiedBinaryOperator {
+    fn from(value: ast::BinaryOperator) -> Self {
+        match value {
+            ast::BinaryOperator::Equal => VerifiedBinaryOperator::Equal,
+            ast::BinaryOperator::NotEqual => VerifiedBinaryOperator::NotEqual,
+            ast::BinaryOperator::Greater => VerifiedBinaryOperator::Greater,
+            ast::BinaryOperator::GreaterEqual => VerifiedBinaryOperator::GreaterEqual,
+            ast::BinaryOperator::Less => VerifiedBinaryOperator::Less,
+            ast::BinaryOperator::LessEqual => VerifiedBinaryOperator::LessEqual,
+            ast::BinaryOperator::Add => VerifiedBinaryOperator::Add,
+            ast::BinaryOperator::Subtract => VerifiedBinaryOperator::Subtract,
+            ast::BinaryOperator::Multiply => VerifiedBinaryOperator::Multiply,
+            ast::BinaryOperator::Divide => VerifiedBinaryOperator::Divide,
+            ast::BinaryOperator::Modulo => VerifiedBinaryOperator::Modulo,
+        }
+    }
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
@@ -25,11 +153,6 @@ pub enum BoundOwner {
     Model(String),
     State(String),
     Function(String),
-}
-
-#[derive(Debug, Copy, Clone, PartialEq, Eq)]
-pub enum VerificationOutcome {
-    StructuralOnly,
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
@@ -63,18 +186,16 @@ pub fn verify(contract: &ContractDefinition) -> Result<VerificationReport, Vec<V
     let mut verifier = Verifier::default();
 
     for model in &contract.models {
-        verifier.verify_bounds(BoundOwner::Model(model.name.clone()), &model.bounds);
+        let scope = scope_from_fields(&model.fields);
+        verifier.verify_bounds(BoundOwner::Model(model.name.clone()), &model.bounds, &scope);
     }
 
     for state in &contract.states {
-        verifier.verify_bounds(BoundOwner::State(state.name.clone()), &state.bounds);
+        verifier.verify_state(state);
     }
 
     for function in &contract.functions {
-        verifier.verify_bounds(
-            BoundOwner::Function(function.name.clone()),
-            &function.bounds,
-        );
+        verifier.verify_function(function);
     }
 
     if verifier.errors.is_empty() {
@@ -91,7 +212,26 @@ struct Verifier {
 }
 
 impl Verifier {
-    fn verify_bounds(&mut self, owner: BoundOwner, bounds: &Bounds) {
+    fn verify_state(&mut self, state: &StateDefinition) {
+        let scope = scope_from_fields(&state.fields);
+        self.verify_bounds(BoundOwner::State(state.name.clone()), &state.bounds, &scope);
+    }
+
+    fn verify_function(&mut self, function: &FunctionDefinition) {
+        let scope = scope_from_params(&function.params);
+        self.verify_bounds(
+            BoundOwner::Function(function.name.clone()),
+            &function.bounds,
+            &scope,
+        );
+    }
+
+    fn verify_bounds(
+        &mut self,
+        owner: BoundOwner,
+        bounds: &Bounds,
+        scope: &HashMap<String, VerifiedType>,
+    ) {
         if !is_valid_span(bounds.span) {
             self.errors.push(VerificationError::new(
                 "bound owner has an invalid span",
@@ -110,22 +250,172 @@ impl Verifier {
                 continue;
             }
 
+            let Some(expression) = self.lower_expression(expression, scope) else {
+                continue;
+            };
+
+            if expression.ty() != VerifiedType::Bool {
+                self.errors.push(VerificationError::new(
+                    "bound expression must lower to bool",
+                    expression.span(),
+                ));
+                continue;
+            }
+
             self.report.checked_bounds += 1;
             match &owner {
                 BoundOwner::Model(_) => self.report.model_bounds += 1,
                 BoundOwner::State(_) => self.report.state_bounds += 1,
                 BoundOwner::Function(_) => self.report.function_bounds += 1,
             }
-            self.report.results.push(BoundVerification {
+            self.report.constraints.push(Constraint {
                 owner: owner.clone(),
                 span,
-                outcome: VerificationOutcome::StructuralOnly,
+                expression,
             });
+        }
+    }
+
+    fn lower_expression(
+        &mut self,
+        expression: &ast::Expression,
+        scope: &HashMap<String, VerifiedType>,
+    ) -> Option<VerifiedExpression> {
+        match expression {
+            ast::Expression::Identifier(identifier) => {
+                let Some(ty) = scope.get(&identifier.text) else {
+                    self.errors.push(VerificationError::new(
+                        format!(
+                            "bound expression refers to unknown identifier {}",
+                            identifier.text
+                        ),
+                        identifier.span,
+                    ));
+                    return None;
+                };
+
+                Some(VerifiedExpression::Identifier {
+                    name: identifier.text.clone(),
+                    ty: ty.clone(),
+                    span: identifier.span,
+                })
+            }
+            ast::Expression::Integer(integer) => Some(VerifiedExpression::IntLiteral {
+                text: integer.text.clone(),
+                span: integer.span,
+            }),
+            ast::Expression::Binary { lhs, op, rhs, span } => {
+                let lhs = self.lower_expression(lhs, scope)?;
+                let rhs = self.lower_expression(rhs, scope)?;
+                let lhs_ty = lhs.ty();
+                let rhs_ty = rhs.ty();
+                let Some(ty) = self.lower_binary_type(*op, &lhs_ty, &rhs_ty, *span) else {
+                    return None;
+                };
+
+                Some(VerifiedExpression::Binary {
+                    lhs: Box::new(lhs),
+                    op: (*op).into(),
+                    rhs: Box::new(rhs),
+                    ty,
+                    span: *span,
+                })
+            }
+        }
+    }
+
+    fn lower_binary_type(
+        &mut self,
+        op: ast::BinaryOperator,
+        lhs_ty: &VerifiedType,
+        rhs_ty: &VerifiedType,
+        span: Span,
+    ) -> Option<VerifiedType> {
+        match op {
+            ast::BinaryOperator::Add
+            | ast::BinaryOperator::Subtract
+            | ast::BinaryOperator::Multiply
+            | ast::BinaryOperator::Divide
+            | ast::BinaryOperator::Modulo => {
+                if lhs_ty.is_numeric() && rhs_ty.is_numeric() {
+                    Some(VerifiedType::merged_numeric(lhs_ty, rhs_ty))
+                } else {
+                    self.errors.push(VerificationError::new(
+                        format!(
+                            "operator {} expects numeric operands",
+                            binary_operator_text(op)
+                        ),
+                        span,
+                    ));
+                    None
+                }
+            }
+            ast::BinaryOperator::Greater
+            | ast::BinaryOperator::GreaterEqual
+            | ast::BinaryOperator::Less
+            | ast::BinaryOperator::LessEqual => {
+                if lhs_ty.is_numeric() && rhs_ty.is_numeric() {
+                    Some(VerifiedType::Bool)
+                } else {
+                    self.errors.push(VerificationError::new(
+                        format!(
+                            "operator {} expects numeric operands",
+                            binary_operator_text(op)
+                        ),
+                        span,
+                    ));
+                    None
+                }
+            }
+            ast::BinaryOperator::Equal | ast::BinaryOperator::NotEqual => {
+                if lhs_ty.is_compatible_with(rhs_ty) {
+                    Some(VerifiedType::Bool)
+                } else {
+                    self.errors.push(VerificationError::new(
+                        format!(
+                            "operator {} expects compatible operands",
+                            binary_operator_text(op)
+                        ),
+                        span,
+                    ));
+                    None
+                }
+            }
         }
     }
 }
 
+fn scope_from_fields(fields: &[FieldDefinition]) -> HashMap<String, VerifiedType> {
+    fields
+        .iter()
+        .map(|field| (field.name.clone(), VerifiedType::from(&field.ty)))
+        .collect()
+}
+
+fn scope_from_params(params: &[ParameterDefinition]) -> HashMap<String, VerifiedType> {
+    params
+        .iter()
+        .map(|param| (param.name.clone(), VerifiedType::from(&param.ty)))
+        .collect()
+}
+
 fn is_valid_span(span: Span) -> bool { span.start < span.end }
+
+fn binary_operator_text(op: ast::BinaryOperator) -> &'static str {
+    match op {
+        ast::BinaryOperator::Equal => "==",
+        ast::BinaryOperator::NotEqual => "!=",
+        ast::BinaryOperator::Greater => ">",
+        ast::BinaryOperator::GreaterEqual => ">=",
+        ast::BinaryOperator::Less => "<",
+        ast::BinaryOperator::LessEqual => "<=",
+        ast::BinaryOperator::Add => "+",
+        ast::BinaryOperator::Subtract => "-",
+        ast::BinaryOperator::Multiply => "*",
+        ast::BinaryOperator::Divide => "/",
+        ast::BinaryOperator::Modulo => "%",
+    }
+}
 
 impl Default for VerificationReport {
     fn default() -> Self {
@@ -134,7 +424,7 @@ impl Default for VerificationReport {
             model_bounds: 0,
             state_bounds: 0,
             function_bounds: 0,
-            results: Vec::new(),
+            constraints: Vec::new(),
         }
     }
 }
@@ -143,17 +433,20 @@ impl Default for VerificationReport {
 mod tests {
     use parser::Span;
     use parser::ast::Expression;
+    use parser::ast::Identifier;
     use parser::ast::IntegerLiteral;
     use semantics::Bounds;
     use semantics::ContractDefinition;
     use semantics::ModelDefinition;
 
     use super::BoundOwner;
-    use super::VerificationOutcome;
+    use super::VerifiedBinaryOperator;
+    use super::VerifiedExpression;
+    use super::VerifiedType;
     use super::verify;
 
     #[test]
-    fn sees_model_state_and_function_bounds() {
+    fn lowers_model_state_and_function_bounds() {
         let source = parser::parse_source(
             r#"
 model Counter {
@@ -181,18 +474,24 @@ must [ amount > 0 ]
         assert_eq!(report.model_bounds, 1);
         assert_eq!(report.state_bounds, 1);
         assert_eq!(report.function_bounds, 1);
-        assert!(report.results.iter().any(|result| {
-            result.owner == BoundOwner::Model("Counter".to_owned())
-                && result.outcome == VerificationOutcome::StructuralOnly
-        }));
-        assert!(report.results.iter().any(|result| {
-            result.owner == BoundOwner::State("Ready".to_owned())
-                && result.outcome == VerificationOutcome::StructuralOnly
-        }));
-        assert!(report.results.iter().any(|result| {
-            result.owner == BoundOwner::Function("increment".to_owned())
-                && result.outcome == VerificationOutcome::StructuralOnly
-        }));
+        assert_has_identifier_constraint(
+            &report,
+            BoundOwner::Model("Counter".to_owned()),
+            "value",
+            VerifiedType::Int,
+        );
+        assert_has_identifier_constraint(
+            &report,
+            BoundOwner::State("Ready".to_owned()),
+            "value",
+            VerifiedType::Int,
+        );
+        assert_has_identifier_constraint(
+            &report,
+            BoundOwner::Function("increment".to_owned()),
+            "amount",
+            VerifiedType::Int,
+        );
     }
 
     #[test]
@@ -221,5 +520,61 @@ must [ amount > 0 ]
                 .iter()
                 .any(|error| error.message == "bound expression has an invalid span")
         );
+    }
+
+    #[test]
+    fn rejects_unknown_internal_identifiers() {
+        let contract = ContractDefinition {
+            models: vec![ModelDefinition {
+                name: "Broken".to_owned(),
+                fields: Vec::new(),
+                bounds: Bounds {
+                    span: Span::new(1, 2),
+                    expressions: vec![Expression::Identifier(Identifier {
+                        text: "missing".to_owned(),
+                        span: Span::new(3, 10),
+                    })],
+                },
+                span: Span::new(1, 2),
+            }],
+            states: Vec::new(),
+            functions: Vec::new(),
+        };
+
+        let errors = verify(&contract).expect_err("contract should fail structural verification");
+
+        assert!(
+            errors
+                .iter()
+                .any(|error| error.message
+                    == "bound expression refers to unknown identifier missing")
+        );
+    }
+
+    fn assert_has_identifier_constraint(
+        report: &super::VerificationReport,
+        owner: BoundOwner,
+        name: &str,
+        ty: VerifiedType,
+    ) {
+        assert!(report.constraints.iter().any(|constraint| {
+            constraint.owner == owner
+                && matches!(
+                    &constraint.expression,
+                    VerifiedExpression::Binary {
+                        lhs,
+                        op: VerifiedBinaryOperator::GreaterEqual | VerifiedBinaryOperator::Greater,
+                        ty: VerifiedType::Bool,
+                        ..
+                    } if matches!(
+                        lhs.as_ref(),
+                        VerifiedExpression::Identifier {
+                            name: ident,
+                            ty: ident_ty,
+                            ..
+                        } if ident == name && ident_ty == &ty
+                    )
+                )
+        }));
     }
 }
